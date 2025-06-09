a432ed4c89154aba978af8c9206b31cc488264e0
dylee <egni1227@naver.com>
2025-06-09 17:00:48 +0900

    feat(SuperContraGame): up, down, left, right 메서드 구현 및 인터페이스 적용

--- src/main/java/com/dylee/learn_spring_framework/AppGamingBasicJava.java
+++ src/main/java/com/dylee/learn_spring_framework/AppGamingBasicJava.java
@@ -8,9 +8,9 @@ public class AppGamingBasicJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		var marioGame = new MarioGame();
-//		var superContraGame = new SuperContraGame();
-		var gameRunner = new GameRunner(marioGame);
+//		var marioGame = new MarioGame();
+		var superContraGame = new SuperContraGame();
+		var gameRunner = new GameRunner(superContraGame);
 		gameRunner.run();
 		
 	}
--- src/main/java/com/dylee/learn_spring_framework/game/SuperContraGame.java
+++ src/main/java/com/dylee/learn_spring_framework/game/SuperContraGame.java
@@ -1,5 +1,16 @@
 package com.dylee.learn_spring_framework.game;
 
-public class SuperContraGame {
-
+public class SuperContraGame implements GamingConsole{
+	public void up() {
+		System.out.println("Up");
+	}
+	public void down() {
+		System.out.println("Sit down");
+	}
+	public void left() {
+		System.out.println("Go back");
+	}
+	public void right() {
+		System.out.println("Shoot a bullet");
+	}
 }
