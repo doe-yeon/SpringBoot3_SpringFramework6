58e5b16c579f80e40588404768f5a1379dc60c9e
dylee <egni1227@naver.com>
2025-06-09 17:31:55 +0900

    feat(PacmanGame): PacmanGame 클래스 추가 및 인터페이스 적용
    - up, down, left, right 메서드 구현

--- src/main/java/com/dylee/learn_spring_framework/AppGamingBasicJava.java
+++ src/main/java/com/dylee/learn_spring_framework/AppGamingBasicJava.java
@@ -2,14 +2,17 @@ package com.dylee.learn_spring_framework;
 
 import com.dylee.learn_spring_framework.game.GameRunner;
 import com.dylee.learn_spring_framework.game.MarioGame;
+import com.dylee.learn_spring_framework.game.PacmanGame;
 import com.dylee.learn_spring_framework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		var game = new MarioGame();
+		//var game = new MarioGame();
 		//var game = new SuperContraGame();
+		var game = new PacmanGame();
+		
 		var gameRunner = new GameRunner(game);
 		gameRunner.run();
 		
