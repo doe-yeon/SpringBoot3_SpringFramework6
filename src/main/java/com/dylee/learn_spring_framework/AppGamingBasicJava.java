5d6765cc6260a03b79ff2c42a92c7f096ce3dc20
dylee <egni1227@naver.com>
2025-06-10 16:21:50 +0900

    chore: AppGamingBasicJava 클래스 리팩토링 전 마지막 저장

--- src/main/java/com/dylee/learn_spring_framework/AppGamingBasicJava.java
+++ src/main/java/com/dylee/learn_spring_framework/AppGamingBasicJava.java
@@ -1,3 +1,4 @@
+//메인클래스
 package com.dylee.learn_spring_framework;
 
 import com.dylee.learn_spring_framework.game.GameRunner;
@@ -11,11 +12,13 @@ public class AppGamingBasicJava {
 		// TODO Auto-generated method stub
 		//var game = new MarioGame();
 		//var game = new SuperContraGame();
-		var game = new PacmanGame();
+		var game = new PacmanGame();	//1: 객체 생성
 		
-		var gameRunner = new GameRunner(game);
+		var gameRunner = new GameRunner(game);	//2: 객체 생성 + 의존성 주입(DI)
+			//GameRunner는 game에 의존한다
 		gameRunner.run();
 		
 	}
 
 }
+ 
--- src/main/java/com/dylee/learn_spring_framework/game/GamingConsole.java
+++ src/main/java/com/dylee/learn_spring_framework/game/GamingConsole.java
@@ -1,5 +1,14 @@
 package com.dylee.learn_spring_framework.game;
 
+/**
+ * 게임 실행기(GameRunner)와 다양한 게임 구현체 간의 결합도를 낮추기 위한 인터페이스입니다.
+ * 
+ * 이 인터페이스를 구현하는 클래스들은 up(), down(), left(), right() 메서드를 통해
+ * 게임 조작 기능을 구현해야 합니다.
+ * 
+ * 인터페이스 도입으로 인해 게임 실행기는 특정 게임 클래스에 직접 의존하지 않고,
+ * 느슨한 결합 구조를 갖게 되어 유지보수와 확장성이 좋아집니다.
+ */
 public interface GamingConsole {
 	void up();
 	void down();
