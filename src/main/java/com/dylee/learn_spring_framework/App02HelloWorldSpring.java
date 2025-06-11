aabb2facc053ee57193c8b745c0a258447ed7171
dylee <egni1227@naver.com>
2025-06-11 17:20:47 +0900

    feat: 스프링 컨테이너 실행 및 빈(name) 조회 출력 기능 구현 - AnnotationConfigApplicationContext로 HelloWorldConfiguration 설정 불러오기 - 컨테이너에서 name 빈(String Ranga)을 꺼내서 출력

--- src/main/java/com/dylee/learn_spring_framework/App02HelloWorldSpring.java
+++ src/main/java/com/dylee/learn_spring_framework/App02HelloWorldSpring.java
@@ -7,9 +7,19 @@ public class App02HelloWorldSpring {
 	public static void main(String[] args) {
 		
 		//1: Launch a Spring Context - 
-
-		//2: Configure the things that we want Spring to manage - @Configuration
 		
+		//-인스턴스 생성
+		var context =
+				//-설정파일 생성
+				new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+
+		
+		//2: Configure the things that we want Spring to manage(HelloWorldConfiguration.java)
+		//-HelloWorldConfiguration: @Configuration
+		//-name: @Bean
+		
+		//3: Retrieving Beans managed by Spring
+		System.out.println(context.getBean("name"));
 		
 		
 	}
--- src/main/java/com/dylee/learn_spring_framework/HelloWorldConfiguration.java
+++ src/main/java/com/dylee/learn_spring_framework/HelloWorldConfiguration.java
@@ -1,6 +1,16 @@
 package com.dylee.learn_spring_framework;
 
-//spring 설정 클래스
-public class HelloWorldConfiguration {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+//spring 설정 클래스
+@Configuration
+public class HelloWorldConfiguration {
+	
+	// 스프링 컨테이너에게 이 메서드에서 생성한 객체를 Bean으로 등록하도록 지시
+    // 아래 name() 메서드는 "Ranga"라는 문자열 객체를 생성하여 빈으로 관리함
+	@Bean
+	public String name() {
+		return "Ranga";
+	}
 }
